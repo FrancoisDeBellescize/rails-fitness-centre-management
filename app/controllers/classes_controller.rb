70f6d2a55f8b2094815427e4814059b7c279e1da
Francois <frdebellescize@hotmail.fr>
2017-05-09 13:41:41 +0100

    clear class

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,74 +1,44 @@
 class ClassesController < ApplicationController
   before_action :set_class, only: [:show, :edit, :update, :destroy]
 
-  # GET /classes
-  # GET /classes.json
-  def index
-    @classes = Classe.all
-  end
-
-  # GET /classes/1
-  # GET /classes/1.json
   def show
   end
 
-  # GET /classes/new
   def new
     @classe = Classe.new
   end
 
-  # GET /classes/1/edit
   def edit
   end
 
-  # POST /classes
-  # POST /classes.json
   def create
     @classe = Classe.new(class_params)
-
-    respond_to do |format|
-      if @classe.save
-        format.html { redirect_to @classe, notice: 'Classe was successfully created.' }
-        format.json { render :show, status: :created, location: @classe }
-      else
-        format.html { render :new }
-        format.json { render json: @classe.errors, status: :unprocessable_entity }
-      end
+    if @classe.save
+      format.html { redirect_to @classe, notice: 'Class was successfully created.' }
+    else
+      format.html { render :new }
     end
   end
 
-  # PATCH/PUT /classes/1
-  # PATCH/PUT /classes/1.json
   def update
-    respond_to do |format|
-      if @classe.update(class_params)
-        format.html { redirect_to @classe, notice: 'Classe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @classe }
-      else
-        format.html { render :edit }
-        format.json { render json: @classe.errors, status: :unprocessable_entity }
-      end
+    if @classe.update(class_params)
+      format.html { redirect_to @classe, notice: 'Class was successfully updated.' }
+    else
+      format.html { render :edit }
     end
   end
 
-  # DELETE /classes/1
-  # DELETE /classes/1.json
   def destroy
     @classe.destroy
-    respond_to do |format|
-      format.html { redirect_to classes_url, notice: 'Classe was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    format.html { redirect_to classes_url, notice: 'Class was successfully destroyed.' }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_class
-      @classe = Classe.find(params[:id])
-    end
+  def set_class
+    @classe = Classe.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def class_params
-      params.require(:classe).permit(:name, :day, :description, :instructor_name, :date_start, :time)
-    end
+  def class_params
+    params.require(:classe).permit(:name, :day, :description, :instructor_name, :date_start, :time)
+  end
 end
